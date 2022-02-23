753a2501891c7eefa7437922b6a1c3081540e6c9
minsoozz <minsoo1604@naver.com>
2022-02-23 20:29:49 +0900

    예제 프로젝트 만들기 v2

--- src/main/java/hello/proxy/app/v1/OrderControllerV1V1Impl.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1V1Impl.java
@@ -2,15 +2,15 @@ package hello.proxy.app.v1;
 
 public class OrderControllerV1V1Impl implements OrderControllerV1 {
 
-  private final OrderServiceV1 orderServiceV1;
+  private final OrderServiceV1 orderService;
 
-  public OrderControllerV1V1Impl(OrderServiceV1 orderServiceV1) {
-    this.orderServiceV1 = orderServiceV1;
+  public OrderControllerV1V1Impl(OrderServiceV1 orderService) {
+    this.orderService = orderService;
   }
 
   @Override
   public String request(String itemId) {
-    orderServiceV1.orderItem(itemId);
+    orderService.orderItem(itemId);
     return "ok";
   }
 
--- src/main/java/hello/proxy/config/AppV1Config.java
+++ src/main/java/hello/proxy/config/AppV1Config.java
@@ -1,11 +1,6 @@
 package hello.proxy.config;
 
-import hello.proxy.app.v1.OrderControllerV1;
-import hello.proxy.app.v1.OrderControllerV1V1Impl;
-import hello.proxy.app.v1.OrderRepositoryV1;
-import hello.proxy.app.v1.OrderRepositoryV1Impl;
-import hello.proxy.app.v1.OrderServiceV1;
-import hello.proxy.app.v1.OrderServiceV1Impl;
+import hello.proxy.app.v1.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
